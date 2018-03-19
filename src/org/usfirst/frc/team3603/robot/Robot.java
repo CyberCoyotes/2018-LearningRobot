93725048df6a4a8a0b6c5081779b72d606b0e136
Cyber Coyotes <Cyber Coyotes@DESKTOP-QOBA06L.reedcityschools.org>
2018-03-19 17:13:24 -0400

    Added custom log code

--- src/org/usfirst/frc/team3603/robot/Robot.java
+++ src/org/usfirst/frc/team3603/robot/Robot.java
@@ -4,13 +4,15 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Random;
 
-import edu.wpi.first.wpilibj.DriverStation;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
-import edu.wpi.first.wpilibj.PowerDistributionPanel;
 
 /**
  * This currently is testing custom log files.
@@ -18,10 +20,10 @@ import edu.wpi.first.wpilibj.PowerDistributionPanel;
  *
  */
 public class Robot extends IterativeRobot {
-	Victor frontLeft = new Victor(0);
-	Victor backLeft = new Victor(1);
-	Victor frontRight = new Victor(2);
-	Victor backRight = new Victor(3);
+	WPI_TalonSRX frontLeft = new WPI_TalonSRX(1);
+	WPI_TalonSRX backLeft = new WPI_TalonSRX(2);
+	WPI_TalonSRX frontRight = new WPI_TalonSRX(3);
+	WPI_TalonSRX backRight = new WPI_TalonSRX(4);
 	MecanumDrive mainDrive = new MecanumDrive(frontLeft, backLeft, frontRight, backRight);
 	
 	Joystick drive = new Joystick(0);
@@ -29,17 +31,28 @@ public class Robot extends IterativeRobot {
 	FileWriter fw;
 	PrintWriter pw;
 	
+	Timer t = new Timer();
+	
 	@Override
 	public void robotInit() {
 		try {
-			File f = new File("/U/matchData/time.txt");
+			Random r = new Random();
+			File f = new File("/U/matchData/log6.txt");
 			if(!f.exists()) {
+				System.out.println("Creating new file...");
 				f.createNewFile();
+				System.out.println("File created");
+			} else {
+				System.out.println("File exists");
 			}
 			fw = new FileWriter(f);
-			pw = new PrintWriter(fw, true);
+			pw = new PrintWriter(fw, false);
+
 		} catch(IOException ex) {
+			System.out.println("File failure...");
 		}
+		t.reset();
+		t.start();
 	}
 	@Override
 	public void autonomousInit() {
@@ -59,7 +72,11 @@ public class Robot extends IterativeRobot {
 		} else {
 			mainDrive.driveCartesian(0, 0, 0);
 		}
-		pw.println(frontLeft.get());
+		pw.print(t.get() + "\t");
+		pw.print(frontLeft.get() + "\t");
+		pw.print(backLeft.get());
+		pw.print("\r\n");
+		pw.flush();
 	}
 
 	@Override
